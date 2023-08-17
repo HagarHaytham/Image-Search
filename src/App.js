3a9a7d65a6bbccc08e8f09d92ac0b9419c16694f
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2023-08-17 05:43:29 +0300

    Implemented search from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState,useEffect } from "react";
 import SearchQuery from "./components/SearchQuery";
 import ImagesGrid from "./components/ImagesGrid";
-import { Key } from "key";
+// import { Key } from "key";
 function App() {
+  const key = "38861307-a7242d37888b818cec2108441";
+  const [images,setImages] = useState([]);
+
+  const searchQuery =async (query)=>{
+    console.log(query.text)
+    const res = await fetch(`https://pixabay.com/api/?key=${key}&$q=${query.text}&per-page=15`,{
+      method:'GET'
+    })
+
+    const data = await res.json()
+    console.log(data.hits)
+    await setImages(data.hits)
+  }
   return (
+
     <div className="App">
       <h2> Image Search Using Pixabay</h2>
-      <SearchQuery/>
+      <SearchQuery onSearch={searchQuery}/>
       <ImagesGrid/>
     </div>
   );
--- src/components/SearchQuery.js
+++ src/components/SearchQuery.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState } from "react"
+const SearchQuery = ({onSearch}) => {
+  const [text,setText] = useState('')
 
-const SearchQuery = () => {
+  const onSubmit = (e) =>{
+      e.preventDefault()
+      if (!text) {
+          alert ('please add Text')
+          return
+      }
+
+      onSearch({text})
+
+      setText('')
+      
+
+  }
   return (
-    <div>SearchQuery</div>
+    <form className ='add-form' onSubmit = {onSubmit}>
+        <div className="form-control">
+            <label>Search Images</label>
+            <input type="text" placeholder="Search " value={text} onChange={(e)=> setText(e.target.value)}/>
+        </div>
+        <input type="submit" value='Search' ></input>
+
+      </form>
   )
 }
 
